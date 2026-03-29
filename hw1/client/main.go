42d1b46cdb434844f6293e2affcd29ef506fcda1
Aleksandr Timokhin <adtimokhin@gmail.com>
2026-03-28 23:41:15 -0500

    added a simple server - client setup where clients can connect to a server

--- hw1/client/main.go
+++ hw1/client/main.go
@@ -1,1 +1,23 @@
-// Your client implementation goes here.
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: client <host:port>")
+		os.Exit(1)
+	}
+
+	conn, err := net.Dial("tcp", os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to", os.Args[1])
+}
--- hw1/server/main.go
+++ hw1/server/main.go
@@ -1,1 +1,33 @@
-// Your server implementation goes here.
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: server <port>")
+		os.Exit(1)
+	}
+
+	ln, err := net.Listen("tcp", ":"+os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer ln.Close()
+
+	fmt.Println("Listening on port", os.Args[1])
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		fmt.Println("User Connected")
+		conn.Close()
+	}
+}
